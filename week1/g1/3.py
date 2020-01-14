d17c7827392db5b5df22f930f30ba23c235c443c
Beisenbek Baisakov <beysenbek@gmail.com>
2020-01-14 20:19:41 +0600

    init

---
+++ week1/g1/3.py
@@ -0,0 +1,6 @@
+def f(N):
+    for i in range(1, N, 2):
+        print(i)
+
+if __name__ == '__main__':
+    f(100)
---
+++ week1/g1/4.py
@@ -0,0 +1,7 @@
+def f(N):
+    for i in range(1, N):
+        if i % 2 == 1:
+            print(i)
+
+if __name__ == '__main__':
+    f(100)
